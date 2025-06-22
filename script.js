58601b7df740d501c7d9b483babc6b9d533ebb8e
sontypo <son94227@gmail.com>
2025-06-22 15:20:51 +0800

    Update README.MD

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ document
 //         animateProgressBar();
 //     });
 
-window.API_URL = "http://127.0.0.1:8000";
+window.API_URL = "http://127.0.0.1:8080";
 
 document
     .getElementById("generate-button")
